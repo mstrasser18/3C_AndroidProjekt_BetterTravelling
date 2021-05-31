ca5422b47b11cd61db83cd158597277ec8de44e7
jthann18 <Thanner.Julian@sus.htl-grieskirchen.at>
2021-05-31 11:02:51 +0200

    Test

---
+++ app/src/main/java/net/htlgrieskirchen/jthanner18/mstrasser18/bettertravelling/MainActivity.java
@@ -0,0 +1,14 @@
+package net.htlgrieskirchen.jthanner18.mstrasser18.bettertravelling;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
