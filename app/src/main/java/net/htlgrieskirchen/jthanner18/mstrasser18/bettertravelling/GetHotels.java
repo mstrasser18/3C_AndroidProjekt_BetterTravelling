7a7322fcdfec23b831e1f941106298147c7d0aec
mstrasser18 <Strasser.Martin@sus.htl-grieskirchen.at>
2021-06-20 23:39:13 +0200

    Hotel-API-Authentifizierung implementiert (Token werden von selbst bezogen)

--- app/src/main/java/net/htlgrieskirchen/jthanner18/mstrasser18/bettertravelling/GetHotels.java
+++ app/src/main/java/net/htlgrieskirchen/jthanner18/mstrasser18/bettertravelling/GetHotels.java
@@ -7,10 +7,6 @@ import android.widget.ArrayAdapter;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.amadeus.Amadeus;
-import com.amadeus.Params;
-import com.amadeus.exceptions.ResponseException;
-import com.amadeus.resources.HotelOffer;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -19,16 +15,19 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
+import javax.net.ssl.HttpsURLConnection;
+
 public class GetHotels extends AsyncTask<String, Integer, JSONObject> {
     private ProgressBar mProgressBar;
     private String lon;
     private String lat;
-    private final String TOKEN;
+    private String token;
     private ArrayList<String> hotelNames;
     private ArrayAdapter<String> adapter;
 
@@ -36,7 +35,7 @@ public class GetHotels extends AsyncTask<String, Integer, JSONObject> {
         this.mProgressBar = pb;
         this.lon = lon;
         this.lat = lat;
-        this.TOKEN = "LoZ91R0mIq6lYRfAaAdsomHnk39x";
+        this.token = null;
         this.hotelNames = hotelNames;
         this.adapter = adapter;
     }
@@ -49,12 +48,12 @@ public class GetHotels extends AsyncTask<String, Integer, JSONObject> {
 
     @Override
     protected JSONObject doInBackground(String... strings) {
-        /*
+        checkToken();
         String sJson = "";
         try {
             HttpURLConnection connection = (HttpURLConnection) new URL("https://test.api.amadeus.com/v2/shopping/hotel-offers?latitude="+lat+"&longitude="+lon+"&radius=5&radiusUnit=KM").openConnection();
             connection.setRequestMethod("GET");
-            connection.setRequestProperty("Authorization", "Bearer "+TOKEN);
+            connection.setRequestProperty("Authorization", "Bearer "+token);
 
             int responseCode = connection.getResponseCode();
             if (responseCode == HttpURLConnection.HTTP_OK) {
@@ -73,12 +72,6 @@ public class GetHotels extends AsyncTask<String, Integer, JSONObject> {
             return null;
         }
 
-         */
-        HotelOffer[] offers = getHotels();
-        for (HotelOffer o: offers) {
-            hotelNames.add(o.getHotel().getName());
-        }
-        adapter.notifyDataSetChanged();
         return null;
     }
 
@@ -86,7 +79,6 @@ public class GetHotels extends AsyncTask<String, Integer, JSONObject> {
     @Override
     protected void onPostExecute(JSONObject json) {
         mProgressBar.setVisibility(View.INVISIBLE);
-    /*
         JSONArray data = null;
         try {
             data = json.getJSONArray("data");
@@ -98,19 +90,65 @@ public class GetHotels extends AsyncTask<String, Integer, JSONObject> {
         } catch (JSONException e) {
             e.printStackTrace();
         }
-     */
 
         super.onPostExecute(json);
     }
 
-    public HotelOffer[] getHotels(){
-        Amadeus a = Amadeus.builder("oIS02vhkm9w4eyVhbyPwaPjkGUrzXBeW", "cHGezOEVzChZJSGM").build();
+    private void checkToken(){
+        if(token == null){
+            token = getNewToken();
+            return;
+        }
         try {
-            HotelOffer[] offers;
-            offers = a.shopping.hotelOffers.get(Params.with("latitude",lat).and("longitude", lon).and("radius", 5).and("radiusUnit", "KM"));
-            return  offers;
-        } catch (ResponseException e) {
+            HttpURLConnection connection = (HttpURLConnection) new URL("https://test.api.amadeus.com/v1/security/oauth2/token/"+token).openConnection();
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(connection.getInputStream()));
+                StringBuilder sb = new StringBuilder();
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                }
+                JSONObject json = new JSONObject(sb.toString());
+                String expired = json.getString("state");
+                int expires_in = json.getInt("expires_in");
+                if(expired.equals("expired")||expires_in < 60){
+                    token = getNewToken();
+                }
+            }
+        } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private String getNewToken(){
+        try {
+            HttpsURLConnection connection = (HttpsURLConnection) new URL("https://test.api.amadeus.com/v1/security/oauth2/token").openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestProperty("Accept-Charset", "UTF-8");
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+
+            String query = "grant_type=client_credentials&client_id=oIS02vhkm9w4eyVhbyPwaPjkGUrzXBeW&client_secret=cHGezOEVzChZJSGM";
+            OutputStream output = connection.getOutputStream();
+            output.write(query.getBytes("UTF-8"));
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(connection.getInputStream()));
+                StringBuilder sb = new StringBuilder();
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                }
+                JSONObject json = new JSONObject(sb.toString());
+                return json.getString("access_token");
+            }
+        }catch (Exception e){
+            return null;
         }
         return null;
     }
